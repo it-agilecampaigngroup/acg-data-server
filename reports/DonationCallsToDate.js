f23178f9a3a7c3f62d8955fe09e302211c8bd460
IT @Agile <it@agilecampaigngroup.com>
2021-03-30 19:55:06 -0400

    Added Donation Calls To Date report.

---
+++ reports/DonationCallsToDate.js
@@ -0,0 +1,135 @@
+'use strict'
+
+const db = require('../db/db')
+const AppError = require('../modules/AppError')
+const ErrorRecorder = require('../db/errorRecorder')
+
+module.exports = async function generate(campaignId, actorId, dateStart, dateEnd) {
+
+    // Build the SQL
+    var sql = buildSQL(campaignId, actorId, dateStart, dateEnd)
+    
+    // For debugging
+    //console.log(sql)
+
+    // Execute the query and return the client
+    try {
+        const dbres = await db.query(sql)
+        if( dbres.rowCount == 1) {
+            const row = dbres.rows[0]
+            return {
+                campaignId: campaignId
+                , actorId: (actorId == 0) ? '' : actorId
+                , dateStart: dateStart
+                , dateEnd: dateEnd
+                , contactRequestCount: row.contact_request_count
+                , totalReponseCount: row.total_reponse_count
+                , positiveReponseCount: row.positive_reponse_count
+                , negativeReponseCount: row.negative_reponse_count
+                , failedAttemptCount: row.failed_attempt_count
+                , rejectedContactCount: row.rejected_contact_count
+                , totalDonationAmount: row.total_donation_amount
+            }
+        }
+        return undefined
+    } catch(e) {
+        ErrorRecorder.recordAppError(new AppError('data-server', 'DonationCallsToDate.js', 'DonationCallsToDate', `Database error generating Donation Calls To Date report: ${sql}`, e))
+        throw new Error(e.message)
+    }
+}
+
+function buildSQL(campaignId, actorId, dateStart, dateEnd) {
+
+    // Increment dateEnd to simplify the query
+    var nextDate = new Date(dateEnd)
+    nextDate.setDate(nextDate.getDate() + 1);
+    dateEnd = new Date(nextDate).toLocaleDateString()
+
+    // Build the SQL
+    var sql = `SELECT\r\n`
+    sql += `(SELECT COUNT(date_created)\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact requested'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `) contact_request_count\r\n`
+
+    sql += `, (SELECT COUNT(date_created)\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact responded'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `) total_reponse_count\r\n`
+
+    sql += `, (SELECT COUNT(date_created)\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact responded'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND contact_result ILIKE 'Positive response'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `) positive_reponse_count\r\n`
+
+    sql += `, (SELECT COUNT(date_created)\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact responded'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND contact_result ILIKE 'Negative response'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `) negative_reponse_count\r\n`
+
+    sql += `, (SELECT COUNT(date_created)\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact attempt failed'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `) failed_attempt_count\r\n`
+
+    sql += `, (SELECT COUNT(date_created)\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact rejected'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `) rejected_contact_count\r\n`
+
+    sql += `, ROUND( (SELECT SUM(CAST(detail->>'amount' AS decimal))\r\n`
+    sql += `FROM base.v_contact_action_log\r\n`
+    sql += `WHERE contact_action ILIKE 'Contact responded'\r\n`
+    sql += `AND contact_method ILIKE 'Phone call'\r\n`
+    sql += `AND contact_reason ILIKE 'Donation request'\r\n`
+    sql += `AND contact_result ILIKE 'Positive response'\r\n`
+    sql += `AND detail::jsonb ? 'amount'\r\n`
+    sql += `AND detail ->> 'amount' ~ '^[0-9\.]+$'\r\n`
+    sql += `AND campaign_id = ${campaignId}\r\n`
+    if( actorId != 0 ) { sql += `AND actor_id = ${actorId}\r\n` }
+    sql += `AND date_created >= '${dateStart}'\r\n`
+    sql += `AND date_created < '${dateEnd}'\r\n`
+    sql += `), 2) total_donation_amount\r\n`
+    sql += `;`
+    
+    return sql
+}
+
--- routes/reports.js
+++ routes/reports.js
@@ -64,10 +64,65 @@ router.get('/SupportResultSummary', utils.authenticateToken, async(req, res) => 
 });
 
 //====================================================================================
-// Support Result Summary Report
+// DonationCallsToDate report
 //
-// Returns the counts of each support result rating in a specified date range for
-// the campaign or an actor.
+// Returns a summary of the contact actions for an actor or a campaign
+// 
+//====================================================================================
+router.get('/DonationCallsToDate', utils.authenticateToken, async(req, res) => {
+
+    try {
+        // Get the actor making the request
+        var actor
+        await utils.getActor(req)
+        .then( got_actor => {
+            actor = got_actor
+        })
+        .catch( (e) => {
+            ErrorRecorder.recordAppError(new AppError('data-server', 'routes/contact.js', 'GET /', 'Unknown error in GET /', e))
+            throw e
+        })
+
+        // Make sure user has not been blocked
+        if( actor.isBlocked === true ) {
+            return res.status(401).send("Forbidden: Actor has been blocked")
+        }
+
+        // The requesting actor must be a Campaign Manager if the report
+        // is not for a specific actor or it's for some other actor
+        var targetActorId = (req.query.actorId === undefined ) ? 0 : parseInt(req.query.actorId)
+        if( actor.isCampaignMgr === false ) {
+            if( targetActorId !== actor.actorId ) {
+               return res.status(401).send("Forbidden: You must be a Campaign Manager to run this report for the campaign or another actor")
+            }
+        }
+
+        // Prepare the arguments
+        var campaignId = parseInt(req.query.campaignId)
+        campaignId = (isNaN(campaignId)) ? actor.campaignId : campaignId
+        var dateStart = (req.query.dateStart === undefined) ? new Date("1/1/2021") : req.query.dateStart
+        dateStart = new Date(dateStart).toLocaleDateString()
+        var dateEnd = (req.query.dateEnd === undefined) ? new Date() : req.query.dateEnd
+        dateEnd = new Date(dateEnd).toLocaleDateString()
+
+        // Generate and return the report
+        try {
+            const report = require('../reports/DonationCallsToDate')
+            return res.status(200).send( await report(campaignId, targetActorId, dateStart, dateEnd) )
+        } catch (e) {
+            return res.status(400).send(`Error generating Donation Calls To Date report: ${e.message}`)
+        }
+
+    } catch(e) {
+        ErrorRecorder.recordAppError(new AppError('data-server', 'routes/reports.js', 'GET /DonationCallsToDate', 'Unknown error in GET /DonationCallsToDate', e))
+        throw e
+    }
+});
+
+//====================================================================================
+// Contact Action Log report
+//
+// Returns the Contact Action Logs for and actor or a campaign
 // 
 //====================================================================================
 router.get('/ContactActionLog', utils.authenticateToken, async(req, res) => {
